ed6ee5ba3bb827258db51cfb42e462e2c64a6207
vgregor <vgregor@pixelfederation.com>
2018-12-18 12:06:35 +0100

    - reformat of the 0_load_data

--- 1_code/0_load_data.R
+++ 1_code/0_load_data.R
@@ -1,7 +1,6 @@
 # ------------
 # Introduction
 # ------------
-
 ## This file loads the v0 dataset and samples a smaller dataset later work
 NAME <- '0_load_data'
 PROJECT <- 'payer_model'
@@ -10,36 +9,25 @@ PROJECT_DIR <- file.path(
   '_PIXEL FEDERATION/GA issues/Games General/GA-972 Payer model'
 )
 
+
 # ------------
 # Preamble
 # ------------
-
-## -------
 ## Imports
-## -------
 source('1_code/00_utils.R')
 
 packageTest('data.table')
 
-
-## --------
 ## Settings
-## --------
 sampleSize <- 100000
 randomSeed <- 1
 
 fileName <- 'ga_972_payer_dataset_v0.rds'
 
-## ---------------------
 ## Set working directory
-## ---------------------
-
 setwd(file.path(PROJECT_DIR, PROJECT))
 
-## ----------------------------------
 ## Set  up pipeline folder if missing
-## ----------------------------------
-
 pipeline <- file.path('2_pipeline', NAME)
 if (!dir.exists(pipeline)) {
   dir.create(pipeline)
@@ -48,10 +36,10 @@ if (!dir.exists(pipeline)) {
   }
 }
 
+
 # ---------
 # Main code
 # ---------
-
 ## Load data from 0_data folder
 dfLoad <- data.table(readRDS(file.path('0_data', fileName)))
 
