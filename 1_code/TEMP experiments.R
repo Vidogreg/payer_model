5c3959564c1261cd138e2a208d3975bbba4c6f83
Vidogreg <vidogreg@gmail.com>
2019-01-07 17:23:00 +0100

    - clean of TEMP exp

--- 1_code/TEMP experiments.R
+++ 1_code/TEMP experiments.R
@@ -59,10 +59,6 @@ trainIndex <- createDataPartition(
 DTtrain <- DT[trainIndex, ]
 DTtest <- DT[-trainIndex, ]
 
-mod1 <- glm(
-  formula = dy_payer ~ dx_pay_count + dx_payer, data = DTtrain,
-  family = 'binomial'
-)
 mod2 <- glm(
   formula = dy_payer ~ ., data = DTtrain,
   family = 'binomial'
@@ -71,90 +67,6 @@ mod2 <- glm(
 mod <- mod2
 print(summary(mod))
 DTtest[, mod1_fit := predict.glm(mod, newdata = DTtest, type = 'response')]
-
-
-# Lift chart
-liftResult <- data.frame(
-  class = factor(DTtest$dy_payer, levels = c(TRUE, FALSE)),
-  model = DTtest$mod1_fit
-)
-liftObj <- lift(class ~ model, data = liftResult)
-print(plot(liftObj, value = 80, auto.key = list(columns = 1, lines = TRUE, points = FALSE)))
-
-# ROC curve
-library(pROC)
-rocObj <- roc(
-  response = factor(DTtest$dy_payer, levels = c(TRUE, FALSE)),
-  predictor = DTtest$mod1_fit,
-  auc = TRUE,
-  plot = TRUE,
-  print.thres = TRUE,
-  print.auc = TRUE
-)
-
-# Confusion matrix for a cut off based on the ROC plot
-cutOff <- 0.013
-DTtest[, mod1_pred := factor(mod1_fit > cutOff)]
-confM <- confusionMatrix(
-  data = DTtest$mod1_pred,
-  reference = DTtest$dy_payer,
-  positive = 'TRUE'
-)
-print(cutOff)
-print(confM$table)
-print(confM$byClass[1])
-print(confM$byClass[5])
-
-
-# More plots - ROC & sensitivity vs. precision
-# cutOffs <- seq(0.001, 0.05, by = 0.001)
-cutOffs <- rocObj$thresholds
-nCutOff <- length(cutOffs)
-TPR <- rep(0, nCutOff)
-TNR <- rep(0, nCutOff)
-FPR <- rep(0, nCutOff)
-PPV <- rep(0, nCutOff)
-posCountRelDiff <- rep(0, nCutOff)
-
-for (i in 1:nCutOff) {
-  DTtest[, mod1_pred := factor(mod1_fit > cutOffs[i])]
-  confM <- table(DTtest$mod1_pred, DTtest$dy_payer)
-  
-  if(length(levels(DTtest$mod1_pred)) < 2) {
-    if(levels(DTtest$mod1_pred) == 'TRUE') {
-      TP <- confM[1, 2]
-      TN <- 0
-      FP <- confM[1, 1]
-      FN <- 0
-    } else {
-      TP <- 0
-      TN <- confM[1, 1]
-      FP <- 0
-      FN <- confM[1, 2]
-    }
-  } else{
-    TP <- confM[2, 2]
-    TN <- confM[1, 1]
-    FP <- confM[2, 1]
-    FN <- confM[1, 2]
-  }
-  
-  # Sensitivity = recall = hit rate = true positive rate
-  TPR[i] <- TP/(TP + FN)
-  # Specificity = selectivity = true negative rate
-  TNR[i] <- TN/(TN + FP)
-  # fallout = false positive rate
-  FPR[i] <- 1 - TNR[i]
-  # precision = positive predictive value
-  PPV[i] <- TP/(TP + FP)
-  # relative difference between count of predicted and actual positives
-  posCountRelDiff[i] <- (TP + FP)/(TP + FN)
-}
-
-plot(FPR, TPR, type = 'l')
-plot(TPR, PPV, type = 'l')
-plot(cutOffs, posCountRelDiff, type = 'l')
-
 
 
 
